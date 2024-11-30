d128cd5a1ef8e64cd982d4e94224bf3a01336627
lukmanxpert <lukmanmiah2004@gmail.com>
2024-12-01 05:24:25 +0600

    added coffees post functionalities

--- src/pages/AddCoffee.jsx
+++ src/pages/AddCoffee.jsx
@@ -3,6 +3,33 @@ import { IoArrowBackOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
 const AddCoffee = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const price = form.price.value;
+        const photo = form.photo.value;
+        const coffee = {name, chef, supplier, taste, category, details, price, photo}
+        fetch('http://localhost:5000/coffees', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(coffee)
+        })
+        .then(res => res.json())
+        .then( data => {
+            if (data.insertedId) {
+                alert("Coffee Added")
+                form.reset();
+            }
+        })
+    }
     return (
         <div className='w-10/12 my-10 mx-auto font-rancho'>
             <Link className='flex items-center gap-1 my-6 text-[#374151] text-2xl font-bold' to='/'><IoArrowBackOutline /> Back To Home</Link>
@@ -12,11 +39,12 @@ const AddCoffee = () => {
                     <p className='px-10'>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
                 </div>
                 <div>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className='grid md:grid-cols-2 gap-4 my-4'>
                             <label>
                                 <p className='py-2 font-raleway'>Name</p>
                                 <input
+                                    name='name'
                                     type="text"
                                     placeholder="Enter Coffee Name"
                                     className="input input-bordered input-md w-full" />
@@ -24,6 +52,7 @@ const AddCoffee = () => {
                             <label>
                                 <p className='py-2 font-raleway'>Chef</p>
                                 <input
+                                    name='chef'
                                     type="text"
                                     placeholder="Enter Coffee Chef"
                                     className="input input-bordered input-md w-full" />
@@ -31,6 +60,7 @@ const AddCoffee = () => {
                             <label>
                                 <p className='py-2 font-raleway'>Supplier</p>
                                 <input
+                                    name='supplier'
                                     type="text"
                                     placeholder="Enter Coffee Supplier"
                                     className="input input-bordered input-md w-full" />
@@ -38,6 +68,7 @@ const AddCoffee = () => {
                             <label>
                                 <p className='py-2 font-raleway'>Taste</p>
                                 <input
+                                    name='taste'
                                     type="text"
                                     placeholder="Enter Coffee Taste"
                                     className="input input-bordered input-md w-full" />
@@ -45,6 +76,7 @@ const AddCoffee = () => {
                             <label>
                                 <p className='py-2 font-raleway'>Category</p>
                                 <input
+                                    name='category'
                                     type="text"
                                     placeholder="Enter Coffee Category"
                                     className="input input-bordered input-md w-full" />
@@ -52,6 +84,7 @@ const AddCoffee = () => {
                             <label>
                                 <p className='py-2 font-raleway'>Details</p>
                                 <input
+                                    name='details'
                                     type="text"
                                     placeholder="Enter Coffee Details"
                                     className="input input-bordered input-md w-full" />
@@ -59,6 +92,7 @@ const AddCoffee = () => {
                             <label>
                                 <p className='py-2 font-raleway'>Price</p>
                                 <input
+                                    name='price'
                                     type="number"
                                     placeholder="Enter Coffee Price"
                                     className="input input-bordered input-md w-full" />
@@ -66,6 +100,7 @@ const AddCoffee = () => {
                             <label>
                                 <p className='py-2 font-raleway'>Photo</p>
                                 <input
+                                    name='photo'
                                     type="text"
                                     placeholder="Enter Photo URL"
                                     className="input input-bordered input-md w-full" />
